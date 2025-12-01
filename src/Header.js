d56c0adc1bc41b3db0e48fced712957b72981e7a
itolkent <itol.kent@gmail.com>
2025-12-01 04:03:10 +0000

    adding logo

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import logo from './img/itol.png'
 const Header = () => {
-  return (
-    <header>
-      <h1>Job Board</h1>
-    </header>
-  );
+    return (
+        <header>
+            <img className='logo-container' src={logo} alt="logo" />
+            <h1>Job Board</h1>
+        </header>
+    );
 };
 
 export default Header;
